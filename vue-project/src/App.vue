fc9bd7c9945048b848d4d3d98791d4752a38bd2a
leejuhwan <seungjoon0324@gmail.com>
2024-10-15 17:20:42 +0900

    click event 활성화

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,32 +1,61 @@
-<script setup>
+<script>
+  export default {
+    data() {
+      return{
+        output : null,
+        prev : null,
+        cur : null,
+        operator : null,
+      };
+    },
+    methods: {
+      operation(e){
+        const number = e.currentTarget.value;
+        if (['+', '-', '*', '/', '='].includes(number)) {
+          //사칙 연산 기호면 연산 로직 구현
+          this.output = null;
+          this.operator = number;
+          this.prev = Number(this.cur);
+          this.cur = null;
+          return;
+        }
 
+        //사용자가 입력한 숫자(실제로는 문자열임)저장
+        this.cur = this.cur === null ? number:(this.cur+=number);
+
+        //입력한 값이 출력칸(output) 안에 표시되도록 output 데이터에 저장
+        this.output = this.cur;
+      },
+    },
+  }
 </script>
 
 <template>
   <div class="calculator">
       <form name="forms">
-        <input type="text" name="output" readonly />
-        <input type="button" class="clear" value="C" />
-        <input type="button" class="operator" value="/" />
-        <input type="button" value="1" />
-        <input type="button" value="2" />
-        <input type="button" value="3" />
-        <input type="button" class="operator" value="*" />
-        <input type="button" value="4" />
-        <input type="button" value="5" />
-        <input type="button" value="6" />
-        <input type="button" class="operator" value="+" />
-        <input type="button" value="7" />
-        <input type="button" value="8" />
-        <input type="button" value="9" />
-        <input type="button" class="operator" value="-" />
-        <input type="button" class="dot" value="." />
-        <input type="button" value="0" />
-        <input type="button" class="operator result" value="=" />
+        <!-- v-model 디렉티브를 사용해 output 데이터를 양방향으로 바인딩 -->
+        <input v-model="output" type="text" name="output" readonly />
+        <input type="button" class="clear" value="C" @click="operation"/>
+        <input type="button" class="operator" value="/" @click="operation"/>
+        <input type="button" value="1" @click="operation"/>
+        <input type="button" value="2" @click="operation"/>
+        <input type="button" value="3" @click="operation"/>
+        <input type="button" class="operator" value="*" @click="operation"/>
+        <input type="button" value="4" @click="operation"/>
+        <input type="button" value="5" @click="operation"/>
+        <input type="button" value="6" @click="operation"/>
+        <input type="button" class="operator" value="+" @click="operation"/>
+        <input type="button" value="7" @click="operation"/>
+        <input type="button" value="8" @click="operation"/>
+        <input type="button" value="9" @click="operation"/>
+        <input type="button" class="operator" value="-" @click="operation"/>
+        <input type="button" class="dot" value="." @click="operation"/>
+        <input type="button" value="0" @click="operation"/>
+        <input type="button" class="operator result" value="=" @click="operation"/>
       </form>
     </div>
 </template>
 
 <style>
-
+@import './assets/style.css';
 </style>
